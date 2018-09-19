bf9013185c7cae63b2b16db28c0608b00cde6db8
joshdbarton <joshdbarton@gmail.com>
2018-09-19 11:56:17 -0500

    removing secret key from production

--- nightstand/settings.py
+++ nightstand/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from .settings_template import secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@d#e2rb%ac2(ma^!t!dlp!)d2i9mc2f*$+c&v6br9rf!inb$6-'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
